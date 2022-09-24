5702f9d3835df2c95493cc627e27c556fe181d2a
timberlakent <Timberlake.NT@gmail.com>
2022-09-24 12:17:38 -0400

    was testing prettier

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
 // External module require express
-const express = require('express')
+const express = require("express");
 
 //req express
-const app = express()
+const app = express();
 
 //req routes
-const routes = require('./routes')
+const routes = require("./routes");
 
 //req db connection
-require('./config/db.connections')
+require("./config/db.connections");
 
 //conection to routes
-app.use('/', require('./routes/dashboard.routes'));
+app.use("/", require("./routes/dashboard.routes"));
 
 //middleware
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //port connection
 const PORT = process.env.PORT || 3000;
 
 //base route
-app.get('/' ,(req, res)=>{
-    res.send("hello group")
-})
-
+app.get("/", (req, res) => {
+  res.send("hello group");
+});
 
 //app.listen for port
-app.listen(PORT, ()=>{
-    console.log(`Listening on http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
+
+
