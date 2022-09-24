1f5ee30ea34b5cd2f08ef0be90f61d3cc983034c
timberlakent <Timberlake.NT@gmail.com>
2022-09-24 17:35:31 -0400

    saving

--- server.js
+++ server.js
@@ -2,17 +2,17 @@
 const express = require("express");
 
 //req cors and adding whitelist
-const cors = require('cors')
-const whitelist = ['http://localhost:3000']
+const cors = require("cors");
+const whitelist = ["http://localhost:3000"];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by cors'))
+      callback(new Error("Not allowed by cors"));
     }
-  }
-}
+  },
+};
 
 //set app to equal express
 const app = express();
@@ -35,9 +35,7 @@ app.use("/", require("./routes/dashboard.routes"));
 //port connection
 const PORT = process.env.PORT || 3000;
 
-
 //app.listen for port
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
 });
-
