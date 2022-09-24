e5be0749c6bd5d10015eac728fb3e6025a68b4a7
timberlakent <Timberlake.NT@gmail.com>
2022-09-24 12:00:36 -0400

    added reqs for db also a link in terminal for our localhost

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 // External module require express
 const express = require('express')
 
+//req express
 const app = express()
 
+//req routes
 const routes = require('./routes')
 
+//req db connection
+require('./config/db.connections')
+
+//conection to routes
 app.use('/', require('./routes/dashboard.routes'));
 
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
+//port connection
 const PORT = process.env.PORT || 3000;
 
+//base route
 app.get('/' ,(req, res)=>{
     res.send("hello group")
 })
 
 
+//app.listen for port
 app.listen(PORT, ()=>{
-    console.log('Listening on', PORT)
+    console.log(`Listening on http://localhost:${PORT}`)
 })
