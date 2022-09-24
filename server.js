5552d4af25652ee407df67899de80d7f6c3315cb
noahsb96 <noahsb96@yahoo.com>
2022-09-24 10:54:00 -0600

    Required dashboard routes in routes/index

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 // External module require express
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-require('dotenv').config()
+require("dotenv").config();
 
-const routes = require('./routes')
+const routes = require("./routes");
 
-app.use('/', require('./routes/dashboard.routes'));
+app.use("/", require("./routes/dashboard.routes"));
 
 const PORT = process.env.PORT || 3003;
 
-require('./config/db.connections.js')
+require("./config/db.connections.js");
 
-app.get('/' ,(req, res)=>{
-    res.send("hello group")
-})
+app.get("/", (req, res) => {
+  res.send("hello group");
+});
 
-app.listen(PORT, ()=>{
-    console.log('Listening on', PORT)
-})
+app.listen(PORT, () => {
+  console.log("Listening on", PORT);
+});
