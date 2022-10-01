458e78b38eca9ace43ea6418b1be9914b93a4fb6
timberlakent <Timberlake.NT@gmail.com>
2022-10-01 17:04:15 -0400

    added labels and inputs to the new form

--- frontend/src/components/student-dashboard-page/NewDashboard.js
+++ frontend/src/components/student-dashboard-page/NewDashboard.js
@@ -2,11 +2,36 @@ import React, { Component } from "react";
 import Header from "../Headers/RootHeader/Header";
 
 export class NewDashboard extends Component {
+  
   render() {
     return (
       <div>
         <Header />
         NewDashboard
+        <form>
+          <label htmlFor="name">Student Name:</label>
+          <input 
+            type = "text"
+            id="studentName"
+            name= "name"
+            placeholder="Students Name"
+            />
+             <label htmlFor="name">Class:</label>
+             < input
+             type="text"
+             id="className"
+             name="class"
+             placeholder="Class Name"
+             />
+              <label htmlFor="name">Enrolled:</label>
+              <input
+              type="boolean"
+              id="isEnrolled"
+              boolean="Enrolled"
+              placeholder="Are they Enrolled?"
+              />
+              <input type='submit' value="Add Student"/>
+        </form>
       </div>
     );
   }
