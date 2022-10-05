b5e7ba5aa911cb9e37d987dab56d0889e61b06f4
usmanaqadri <usmanaqadri@gmail.com>
2022-10-05 15:45:19 -0400

    delete button working

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ const PORT = process.env.BACKEND_PORT || 3000;
 app.use(
   cors({
     origin: ["http://localhost:3006"],
-    methods: ["GET", "POST", "PUT"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
