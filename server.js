5c12dd9c13f5ceefa314b678a41aea80c172a100
usmanaqadri <usmanaqadri@gmail.com>
2022-10-07 08:32:26 -0400

    updated req to res

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
   baseUrl = "https://student-dashboards.herokuapp.com";
   app.use(express.static("build"));
   app.get("*", (req, res) => {
-    req.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
