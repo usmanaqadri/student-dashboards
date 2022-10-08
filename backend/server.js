294dd58056a4d1ce0436be26e8ba354d6a367b1c
usmanaqadri <usmanaqadri@gmail.com>
2022-10-08 09:45:28 -0400

    some more styling

--- backend/server.js
+++ backend/server.js
@@ -10,44 +10,11 @@ const SESSION_SECRET = process.env.SESSION_SECRET;
 const cookieParser = require("cookie-parser");
 const path = require("path");
 // base URL
-let baseUrl =
-  process.env.NODE_ENV === "development"
-    ? "localhost"
-    : "https://student-dashboards.herokuapp.com";
+let baseUrl = "http://localhost";
 //port connection
 const PORT = process.env.PORT || process.env.BACKEND_PORT || 3009;
 
-//adding whitelist
-// const whitelist = [
-//   `http://localhost:${PORT}`,
-//   `http://localhost:${process.env.FRONTEND_PORT}`,
-// ];
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (!origin) {
-//       //for bypassing postman req with  no origin
-//       return callback(null, true);
-//     }
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-// };
-
 //middleware
-// app.use(cors(corsOptions));
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3006",
-      "https://student-dashboards.herokuapp.com",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
@@ -57,23 +24,27 @@ app.use(
     saveUninitialized: false,
   })
 );
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   next();
-// });
 app.use(cookieParser());
-
-const routes = require("./routes");
+app.use(
+  cors({
+    origin: [
+      `${baseUrl}:${process.env.FRONTEND_PORT}`,
+      "https://student-dashboards.herokuapp.com",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 //req db connection
 require("./config/db.connections");
 
 //conection to routes
+const routes = require("./routes");
 app.use("/", require("./routes/dashboard.routes"));
 app.use("/", require("./routes/users.routes"));
 
-//using build folder
-
+//using build folder when going to production
 if (process.env.NODE_ENV === "production") {
   baseUrl = "https://student-dashboards.herokuapp.com";
   app.use(express.static(path.join(__dirname, "build")));
--- frontend/src/components/Headers/RootHeader/Header.js
+++ frontend/src/components/Headers/RootHeader/Header.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import Nav from "./Nav";
 
 export class Header extends Component {
   render() {
     return (
       <header>
+        <Link style={{ textDecoration: "none" }} to={`/`}>
+          <h1>Student Dashboards</h1>
+        </Link>
         <Nav />
-        <h1>Student Dashboards</h1>
       </header>
     );
   }
